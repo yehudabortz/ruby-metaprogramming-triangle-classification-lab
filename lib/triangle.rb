015e74abb73baa09c85b5f5436ca6dd243846088
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-19 11:46:16 -0600

    commit tree 5ad5dcaebbef1cd97b852f305fa42ad2bd3f3e16

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    not_valid
+    if @l1 == @l2 && @l3 == @l1
+      return :equilateral
+    elsif @l1 == @l2 || @l2 == @l3 || @l3 == @l1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def not_valid
+    if @l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l3 + @l1 <= @l2 || @l1 <= 0 || @l2 <= 0 || @l3 <= 0
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
+
+
